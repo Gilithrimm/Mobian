a567c84539041af22e1433de6c84e26cd48503e4
Gilithrimm <jaspidan@wp.pl>
2021-09-30 19:13:06 +0200

    not too important, not too unimportant

--- src/com/safenar/lang/Methods.java
+++ src/com/safenar/lang/Methods.java
@@ -3,7 +3,6 @@ package com.safenar.lang;
 import com.safenar.Main;
 
 public class Methods {
-
     public static void help() {
         for (Keyword keyword: Main.keywords) {
             Main.println(keyword.getName()+"-"+keyword.getDescription());
