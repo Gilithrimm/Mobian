9d2af89514ebf933529c4e3ba8574e2be4c05fbe
Gilithrimm <jaspidan@wp.pl>
2021-10-15 23:43:51 +0200

    lemme tell ya a secret: i also dunno wtaf is this shit

--- src/com/safenar/swing/MyFrame.java
+++ src/com/safenar/swing/MyFrame.java
@@ -6,12 +6,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MyFrame extends JFrame {
+    static final GraphicsEnvironment ge=GraphicsEnvironment.getLocalGraphicsEnvironment();
+    static final Rectangle frameSize=ge.getMaximumWindowBounds();
     public MyFrame() throws HeadlessException {
         super("Mobian Lang ver. "+ Main.version);
-        JPanel myPanel=new MyPanel();
-        add(myPanel);
-        setExtendedState(JFrame.MAXIMIZED_BOTH);
-        setVisible(true);
+        setLayout(new FlowLayout());//2.WTF 3.go [...] urself if u're not working
+        getContentPane().setBackground(Color.darkGray);//1.not working, stackoverflow sucks 4.where tf did that gray shit come from?
+        MyPanel myPanel=new MyPanel();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().setFullScreenWindow(this);//fullscreen
+        setSize(frameSize.getSize());
+        setVisible(true);
+        add(myPanel);
     }
+
 }
--- src/com/safenar/swing/MyPanel.java
+++ src/com/safenar/swing/MyPanel.java
@@ -5,19 +5,23 @@ import com.safenar.Main;
 import javax.swing.*;
 import java.awt.*;
 
+import static com.safenar.swing.MyFrame.frameSize;
+
 public class MyPanel extends JPanel {
     private static final JTextArea PRINT_AREA =new JTextArea();
+    public static Font mono=new Font(Font.MONOSPACED, Font.PLAIN,12);
 
     public MyPanel() {
         super();
         display();
     }
     private void display(){
-        PRINT_AREA.setBackground(Color.DARK_GRAY);
+        PRINT_AREA.setBackground(Color.BLACK);
         PRINT_AREA.setCaretColor(Color.WHITE);
         PRINT_AREA.setForeground(Color.WHITE);
-        PRINT_AREA.setFont(new Font(Font.MONOSPACED, Font.PLAIN,12));
-        PRINT_AREA.setSize(Main.frame[0].getSize());
+        PRINT_AREA.setFont(mono);
+        PRINT_AREA.setSize(frameSize.getSize());
+        Main.println(frameSize.getSize());
         this.add(PRINT_AREA);
     }
     //KeyListener?
